6a69ebd17309f9f12a7bc9b5d4f6906d77c4fef0
ilia <53568333+ivanovilia96@users.noreply.github.com>
2023-11-16 18:29:57 +0300

    Добавил дополнительные поля в User структуру

--- user.go
+++ user.go
@@ -22,7 +22,16 @@ import (
 
 // User encapsulates user data returned from ipa user commands
 type User struct {
-	UUID             string              `json:"ipauniqueid"`
+	UUID              string `json:"ipauniqueid"`
+	Employeenumber    string `json:"employeenumber"`
+	CN                string `json:"cn"`
+	Gecos             string `json:"gecos"`
+	Title             string `json:"title"`
+	DepartmentNumber  string `json:"departmentnumber"`
+	L                 string `json:"l"`
+	EmployeeID        string `json:"employeeID"`
+	PreferredLanguage string `json:"preferredlanguage"`
+
 	DN               string              `json:"dn"`
 	First            string              `json:"givenname"`
 	Last             string              `json:"sn"`
